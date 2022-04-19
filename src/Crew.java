8a50be6bd499568fdcea2b8002835439e4f838f4
Abby Bock <abock_02@arcadia.edu>
2022-04-19 16:19:03 -0400

    save

--- src/Crew.java
+++ src/Crew.java
@@ -4,10 +4,19 @@ public class Crew {
     String name;
     String role;
     String ID;
+    int sus_lvl = 0;
+
     Crew(String r, String n, String i){
         this.name = n;
         this.role = r; 
         this.ID = i;
+        char s = role.charAt(0);
+        // switch(s){
+        //     case 'C':
+        //     {
+
+        //     }
+        // }
     }
     @Override
     public String toString() {
